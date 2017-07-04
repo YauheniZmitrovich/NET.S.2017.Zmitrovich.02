671eb3d4cfe26fcb3ddff0fff4506d938c2687c2
zmitr-gen <zmitrovichevgeny@gmail.com>
2017-07-05 00:27:11 +0300

    Added logic and unit tests. Was used Microsoft unit testing framework.

---
+++ Logic.Tests/IntArraySearcherTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Logic.IntArraySearcher;
+
+namespace Logic.Tests
+{
+    [TestClass]
+    public class IntArraySearcherTests
+    {
+
+        #region TestContextDeclaring
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+        #endregion
+
+
+        #region GoodArrayTests
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "|DataDirectory|\\GoodArrays.xml",
+            "TestCase",
+            DataAccessMethod.Sequential)]
+        [DeploymentItem("Logic.Tests\\GoodArrays.xml")]
+        [TestMethod]
+        public void GetIndexLRSumEquals_GoodArray_ReturnsExpectedIndex()
+        {
+            // arrange  
+            int[] arr = TestContext.DataRow["array"].ToString().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            int expectedIndex = Convert.ToInt32(TestContext.DataRow["expectedIndex"]);
+
+
+            // act  
+            int resIndex = GetIndexLRSumEquals(arr);
+
+            // assert 
+            Assert.AreEqual(expectedIndex, resIndex);
+        }
+
+        #endregion
+
+
+        [TestMethod]
+        public void GetIndexLRSumEquals_IndexNotFounded_ReturnsMinus1()
+        {
+            // arrange  
+            int[] arr = { 0, 6, 2, 3, 4 };
+            int expectedIndex = -1;
+
+            // act  
+            int resIndex = GetIndexLRSumEquals(arr);
+
+            // assert 
+            Assert.AreEqual(expectedIndex, resIndex);
+        }
+
+
+        #region ExcArrayTests
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetIndexLRSumEquals_NullArray_ShouldThrowArgumentNullException()
+        {
+            // arrange  
+            int[] arr = null;
+
+            // act  
+            GetIndexLRSumEquals(arr);
+
+            // assert is handled by ExpectedException  
+        }
+
+
+        #region ArgExceptionTests
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetIndexLRSumEquals_ArrayLengthMoreThen999_ShouldThrowArgumentException()
+        {
+            // arrange  
+            int[] arr = new int[1000];
+            for (int i = 0; i < 1000; i++)
+                arr[i] = i;
+            // act  
+            GetIndexLRSumEquals(arr);
+
+            // assert is handled by ExpectedException  
+        }
+
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "|DataDirectory|\\ArgExcArrays.xml",
+            "TestCase",
+            DataAccessMethod.Sequential)]
+        [DeploymentItem("Logic.Tests\\ArgExcArrays.xml")]
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetIndexLRSumEquals_IncorrectLengthOfArray_ShouldThrowArgumenException()
+        {
+            // arrange  
+            int[] arr = TestContext.DataRow["array"].ToString().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            // act  
+            GetIndexLRSumEquals(arr);
+
+            // assert is handled by ExpectedException  
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ Logic/IntArraySearcher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    /// <summary>
+    /// The class helps to get diferrent info about array.
+    /// </summary>
+    public static class IntArraySearcher
+    {
+        /// <summary>
+        /// The method searches the index of an element where the sum before 
+        /// equals to the sum after the element in the array.
+        /// </summary>
+        /// <param name="arr">
+        /// The one-dimensional int array where we search our element.
+        /// </param>
+        /// <returns>
+        /// Index where the sum before equals to the sum after element.
+        /// </returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static int GetIndexLRSumEquals(int[] arr)
+        {
+            if (arr == null)
+                throw new ArgumentNullException();
+
+            if (arr.Length < 3 || arr.Length >= 1000)
+                throw new ArgumentException();
+
+            int sum = arr.Sum();
+
+            int i, currSum = 0;
+            for (i = 0; i < arr.Length - 1; i++)
+            {
+                currSum += arr[i];
+                if ((currSum * 2 + arr[i + 1]) == sum)
+                    return i + 1;
+            }
+
+            return -1;
+        }
+    }
+
+
+}
